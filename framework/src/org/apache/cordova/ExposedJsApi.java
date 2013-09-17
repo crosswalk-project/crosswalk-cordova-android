c4ddc96caef682806fe5effdcebf357ad36760e7
Gao Chun <chun.gao@intel.com>
2013-09-17 17:34:50 +0800

    Add Looper to the threat that plugin runs in.
    This fixes the issue that Geolocation plugin does not work.
    In XWalk, the thread which ExposedJsApi.exec() runs in does not contain
    a message loop. As android.location.LocationManager need a loop to
    process message, we have to prepare a Looper manually.

--- framework/src/org/apache/cordova/ExposedJsApi.java
+++ framework/src/org/apache/cordova/ExposedJsApi.java
@@ -23,6 +23,8 @@ import org.chromium.content.browser.JavascriptInterface;
 import org.apache.cordova.PluginManager;
 import org.json.JSONException;
 
+import android.os.Looper;
+
 /**
  * Contains APIs that the JS can call. All functions in here should also have
  * an equivalent entry in CordovaChromeClient.java, and be added to
@@ -42,6 +44,10 @@ import org.json.JSONException;
     public String exec(String service, String action, String callbackId, String arguments) throws JSONException {
         // If the arguments weren't received, send a message back to JS.  It will switch bridge modes and try again.  See CB-2666.
         // We send a message meant specifically for this case.  It starts with "@" so no other message can be encoded into the same string.
+        if (Looper.myLooper() == null) {
+            Looper.prepare();
+        }
+
         if (arguments == null) {
             return "@Null arguments.";
         }
