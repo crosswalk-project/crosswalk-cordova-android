0c2c1c3e37f9d97707ff5fd7cbc588c93abc90f0
Junmin Zhu <junmin.jzhu@gmail.com>
2013-10-16 23:32:47 +0800

    Add more detail comments and TODO for file transfer plugin crash issue.

--- framework/src/org/apache/cordova/CordovaPlugin.java
+++ framework/src/org/apache/cordova/CordovaPlugin.java
@@ -50,6 +50,13 @@ public class CordovaPlugin {
         this.preferences = preferences;
         initialize(cordova, webView);
         pluginInitialize();
+        // File Transfer API implementation leverages the android.webkit.CookieManager.
+        // But trying to getinstance() of CookieManager before the webview
+        // instantiated would cause crash. In the cordova with xwalk backend,
+        // there doesn't exist webview. From the android official document
+        // (http://developer.android.com/reference/android/webkit/CookieManager.html),
+        // it requires to call following API first.
+        // TODO: add condition only for xwalk backend when dynamic switch is ready.
         CookieSyncManager.createInstance(cordova.getActivity());
     }
 
