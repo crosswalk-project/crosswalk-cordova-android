95b7fb17f24616f6cb6aab7be180bd0b2b6ada57
Ningxin Hu <ningxin.hu@intel.com>
2013-10-28 09:43:08 +0800

    Enable the pause and resume timers based on XWalkView.
    Enable them since XWalkView has the APIs.

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -734,8 +734,7 @@ public class CordovaWebView extends XWalkView {
         // If app doesn't want to run in background
         if (!keepRunning) {
             // Pause JavaScript timers (including setInterval)
-        	// TODO(nhu): implement it in XWalkView via ContentViewStatics
-            //this.pauseTimers();
+            this.pauseTimers();
         }
         paused = true;
    
@@ -752,8 +751,7 @@ public class CordovaWebView extends XWalkView {
         }
 
         // Resume JavaScript timers (including setInterval)
-        // TODO(nhu): implement it in XWalkView via ContentViewStatics
-        //this.resumeTimers();
+        this.resumeTimers();
         paused = false;
     }
     
