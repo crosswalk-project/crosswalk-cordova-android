27b0c0a64c6488529cb3d07243291820e9cf2002
Shiliu Wang <shiliu.wang@intel.com>
2013-10-30 14:00:52 +0800

    Add empty implementation for loadDataForTest in CordovaWebView
    Otherwise, build is blocked for abstract function not implemented.

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -1056,6 +1056,10 @@ public class CordovaWebView extends XWalkView implements XWalkRuntimeViewProvide
     }
 
     @Override
+    public void loadDataForTest(String data, String mimeType, boolean isBase64Encoded) {
+    }
+
+    @Override
     public void setCallbackForTest(Object callback) {
     }
 }
