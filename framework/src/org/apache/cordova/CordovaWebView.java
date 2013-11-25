066cf944eaa72264dfad0517d7edacde1209cfeb
Ningxin Hu <ningxin.hu@intel.com>
2013-11-25 22:34:47 +0800

    Not to enable remote debugging by default.
    BUG=https://crosswalk-project.org/jira/browse/XWALK-457

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -195,9 +195,6 @@ public class CordovaWebView extends XWalkView {
     @SuppressLint("SetJavaScriptEnabled")
     @SuppressWarnings("deprecation")
     private void initWebViewSettings() {
-    	// Enable remote debugging server
-    	enableRemoteDebugging();
-    	
         this.setInitialScale(0);
         this.setVerticalScrollBarEnabled(false);
         // TODO: The Activity is the one that should call requestFocus().
