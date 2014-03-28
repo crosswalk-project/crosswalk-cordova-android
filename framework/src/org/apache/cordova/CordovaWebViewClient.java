4b45a478065d7a43e8539b29e52aec441de725d7
Yongsheng Zhu <yongsheng.zhu@intel.com>
2014-03-28 10:21:01 +0800

    Fix the compile error caused by the change of onReceivedSslError.
    The parameters of XWalkClient.onReceivedSslError are changed due to
    a new fix. Change it as well here to pass the compilation.
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-1287

--- framework/src/org/apache/cordova/CordovaWebViewClient.java
+++ framework/src/org/apache/cordova/CordovaWebViewClient.java
@@ -39,6 +39,7 @@ import android.util.Log;
 import android.view.View;
 //import android.webkit.HttpAuthHandler;
 //import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
 import android.webkit.WebResourceResponse;
 //import android.webkit.WebView;
 //import android.webkit.WebViewClient;
@@ -528,7 +529,7 @@ public class CordovaWebViewClient extends XWalkClient {
      */
     @TargetApi(8)
     @Override
-    public void onReceivedSslError(XWalkView view, SslErrorHandler handler, SslError error) {
+    public void onReceivedSslError(XWalkView view, ValueCallback<Boolean> callback, SslError error) {
 
         final String packageName = this.cordova.getActivity().getPackageName();
         final PackageManager pm = this.cordova.getActivity().getPackageManager();
@@ -538,15 +539,15 @@ public class CordovaWebViewClient extends XWalkClient {
             appInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
             if ((appInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
                 // debug = true
-                handler.proceed();
+                callback.onReceiveValue(true);
                 return;
             } else {
                 // debug = false
-                super.onReceivedSslError(view, handler, error);
+                super.onReceivedSslError(view, callback, error);
             }
         } catch (NameNotFoundException e) {
             // When it doubt, lock it out!
-            super.onReceivedSslError(view, handler, error);
+            super.onReceivedSslError(view, callback, error);
         }
     }
 
