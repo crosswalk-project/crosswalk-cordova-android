2df5b995692f72c12c9b5b3abc860246af2024ab
chauthai <chau.thai@gmail.com>
2014-08-11 16:44:04 +0200

    Fixed CordovaActivity.java toogleFullscreen method
    On KitKat the cordova app would always go to full screen after resume.
    Commenting out the flags will have the fullscreen behaviour you set in the /res/config.xml on the fullscreen flag.

--- framework/src/org/apache/cordova/CordovaActivity.java
+++ framework/src/org/apache/cordova/CordovaActivity.java
@@ -278,11 +278,7 @@ public class CordovaActivity extends Activity implements CordovaInterface {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             final int uiOptions =
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
 
             window.getDecorView().setSystemUiVisibility(uiOptions);
         } else {
