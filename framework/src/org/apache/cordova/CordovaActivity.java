c9e30e77cf71da2d189faecd861c29c04a1548f6
fujunwei <junwei.fu@intel.com>
2014-12-19 15:12:31 +0800

    Fix immersive full screen mode doesn't recover
    Immersive Fullscreen mode doesn´t recover after changing the volume on the device.
    Refer https://github.com/libgdx/libgdx/issues/1468 to fix this issue.
    
    BUG=XWALK-2424

--- framework/src/org/apache/cordova/CordovaActivity.java
+++ framework/src/org/apache/cordova/CordovaActivity.java
@@ -222,6 +222,17 @@ public class CordovaActivity extends Activity implements CordovaInterface {
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
         } else if (preferences.getBoolean("Fullscreen", false)) {
             toggleFullscreen(getWindow());
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            setSystemUiVisibilityMode(getWindow());
+                        }
+                    }
+                });
+            }
         } else {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
@@ -300,7 +311,15 @@ public class CordovaActivity extends Activity implements CordovaInterface {
     @SuppressLint("NewApi")
     private void toggleFullscreen(Window window) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            final int uiOptions =
+            setSystemUiVisibilityMode(window);
+        } else {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+    }
+
+    private void setSystemUiVisibilityMode(Window window) {
+        final int uiOptions =
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -308,11 +327,7 @@ public class CordovaActivity extends Activity implements CordovaInterface {
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
-            window.getDecorView().setSystemUiVisibility(uiOptions);
-        } else {
-            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
+        window.getDecorView().setSystemUiVisibility(uiOptions);
     }
 
     /**
@@ -1021,6 +1036,17 @@ public class CordovaActivity extends Activity implements CordovaInterface {
                 if(getBooleanProperty("FullScreen", false))
                 {
                     toggleFullscreen(splashDialog.getWindow());
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        splashDialog.getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                            @Override
+                            public void onSystemUiVisibilityChange(int visibility) {
+                                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                                    setSystemUiVisibilityMode(splashDialog.getWindow());
+                                }
+                            }
+                        });
+                    }
                 }
 
                 splashDialog.setContentView(splashLayout);
